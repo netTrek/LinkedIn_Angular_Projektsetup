2ae78ef582f034e1654302993f1a451fc0ed5e5c
saban <us@netTrek.de>
2018-05-17 09:29:36 +0200

    03_11_typings

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'in-root',
   templateUrl: './app.component.html',
@@ -7,4 +8,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'in';
+  constructor () {
+    $('body').css('backgroundColor', 'red');
+  }
 }
