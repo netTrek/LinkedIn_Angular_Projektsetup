8144254f3fdb8c8c28bb6ac671c98a784ba13059
saban <us@netTrek.de>
2018-05-19 16:11:53 +0200

    10_08_pipesErstellen

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -3,12 +3,13 @@ import { CommonModule } from '@angular/common';
 import { UserComponent } from './user/user.component';
 import { UserListComponent } from './user-list/user-list.component';
 import { UserTypeDirective } from './user-type.directive';
+import { UserRolePipe } from './user-role.pipe';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [UserComponent, UserListComponent, UserTypeDirective],
-  exports: [UserComponent, UserListComponent, UserTypeDirective]
+  declarations: [UserComponent, UserListComponent, UserTypeDirective, UserRolePipe],
+  exports: [UserComponent, UserListComponent, UserTypeDirective, UserRolePipe]
 })
 export class UserModule { }
